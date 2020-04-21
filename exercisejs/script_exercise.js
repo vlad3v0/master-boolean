62ba522e04b3b858b158076fe11543ac5d16e1cd
vladevgeorge <vbt.george@gmail.com>
2020-04-21 16:44:30 +0200

    ex-2 complete

--- exercisejs/script_exercise.js
+++ exercisejs/script_exercise.js
@@ -2,242 +2,6 @@
 
 //00000000000000000000000000000000000000000000000000000000000000000000
 //00----------------------------------------------------------------00
-//00---------------- EX Javascripts.info ---------------------------00
-//00----------------------------------------------------------------00
-//00000000000000000000000000000000000000000000000000000000000000000000
-
-// ---------------- EX Controllo ---------------------------
-
-
-// let domanda = "Qual'è il nome ufficiale di javascript?";
-
-// let risposta = prompt(domanda);
-
-// console.log(risposta);
-// risposta = risposta.toUpperCase();
-
-// for (let i = 0; i < 1; i++) {
-//     if (risposta == "") {
-//         alert("inserisci qualcosa scemo!");
-//         break;
-//     }
-//     if (risposta == "ECMASCRIPT") {
-//         alert("Right!");
-        
-//     }  
-//     else
-//     {
-//         alert("You don't Know?");
-//     }
-    
-// }
-
-// --------------------------------------------------------------
-
-// let age = prompt('age?', 18);
-// let message = (age < 3) ? 'Hi, baby!' :
-//   (age < 18) ? 'Hello!' :
-//   (age < 100) ? 'Greetings!' :
-//   'What an unusual age!';
-
-// alert( message );
-
-//--------------------------------------------
-
-// let number = prompt("inserisci un numero!");
-// number = Number(number);
-
-// console.log(number);
-// if (!number) {
-//     alert("Devi inserire un numero scemo!");
-// }
-// else if (number > 0) {
-//     alert("1");
-// }
-// else if (number <= 0) {
-//     alert("-1");
-// }
-
-//--------------------------------------------
-
-
-
-// let result =  (a + b < 4) ? "Below" : "Over"; 
-
-// // console.log(result);
-
-// let message = (login == "Director") ? "Greetings" : (login == "") ? "no login" : (login == "") ?  : "" 
-
-//--------------------------------------------
-
-// let age = prompt("");
-
-// age = Number();
-
-// console.log(age);
-
-
-// if ((age == 0) || (age < 0)) {
-//     alert("devi inserire un numero");
-//     age = prompt("");
-// }
-// if ((age >= 14) && (age <= 90)) { 
-//     alert("Access Ok")
-// }
-// else {
-//     alert("Stop")
-// }
-
-// console.log(age);
-
-// ---------- CONTROLLO VERIFICA LOGIN ---------------
-
-// let requireAccess = prompt("Ciao chi sei?");
-// console.log(requireAccess);
-
-
-// if ((requireAccess == "") || (requireAccess == null )) {
-//     alert("Riprova");
-// }
-// else if (requireAccess == "Utente") {
-//     let password = prompt("inserisci la password");
-//     console.log(password);
-//     if ((password == "") || (password == null )) {
-//         alert("Devi inserire la password");
-//     }
-//     else if (password == "TheMaster") {
-//         alert("Accesso Autorizzato! Benvenuto Utente =) !")
-//     } 
-//     else {
-//         alert("Non ti Conosco!!!")
-//     } 
-// }  
-// else {
-//     alert("Inserisci un Utente Valido!!!")
-// }
-
-
-
-// let num;
-
-// num = prompt("inserisci un numero maggiore di 100", 0);
-
-// while (num <= 100 ) {
-//    if (num > 100) {
-//        alert("Corretto");
-//        break;
-//    } else 
-//    {
-//        alert("Ripeti")
-//    }
-//    num = prompt("numero maggiore di 100", 0);
-// }
-
-// let num;
-
-// do {
-//   num = prompt("Enter a num greater than 100?", 0);
-// } while (num <= 100 && num);
-
-
-
-// ---------- EX Cicli ---------------
-
-// for (let i = 0; i < 3; i++) {
-//     alert( 'Ciao' );
-//   }
-  
-//   let i = 0;
-
-//   while (i < 3) {
-//       alert(i);
-//       i++;
-//   }
-
-// ---------- EX Cicli ---------------
-
-// let n = 10;
-
-// nextprime:
-// for (let i = 2; i <= n; i++) {
-    
-//     for (let j = 0; j < i; j++) {
-//         if (i % 2 == 0) {
-//             continue nextprime; // inserire etichetta per uscire dal ciclo
-//         }
-         
-//     }
-//     document.write(i + "<br>");   
-// }
-
-// -------------- Uscire da un ciclo con un etichetta: ---------------
-
-// outer: for (let i = 0; i < 3; i++) {
-
-//     for (let j = 0; j < 3; j++) {
-  
-//       let input = prompt(`Value at coords (${i},${j})`, '');
-  
-//       // if an empty string or canceled, then break out of both loops
-//       if (!input) break outer; // (*)
-  
-//       // do something with the value...
-//     }
-//   }
-//   alert('Done!');
-
-// -------------- while function ---------------
-
-
-// let i = 0;
-// while (++i < 5) console.log(i);
-
-
-// let i = 0;
-// while (i++ < 5) console.log(i);
-
-// for (let i = 0; i < 5; i++) console.log( i );
-// console.log('-------/Spazio\\----------');
-
-// for (let i = 0; i < 5; ++i) console.log( i );
-
-// for (let i = 0; i < 3; i++) {
-//     console.log( `number ${i}!` );
-//   }
-
-//   console.log('-------------.-.-.-.-.-.-------------');
-  
-  // let i = 0;
-  // while (i < 3) {
-    
-  //   i++;
-  //   console.log('number '+ i + '!')
-      
-  // }
-
-  
-// ---------------------- EX Js.info - Worm  SWITCH CASEk--------------
-
-// let risposta = prompt("quanto è lungo?");
-// let worm = risposta;
-
-// switch (worm) {
-//   case '3':
-//     alert( 'Too small' );
-//     break;
-//   case '4':
-//     alert( 'Exactly!' );
-//     break;
-//   case '5':
-//     alert( 'Too large' );
-//     break;
-//   default:
-//     alert( "mmm troppo lontano" );
-// }
-
-
-//00000000000000000000000000000000000000000000000000000000000000000000
-//00----------------------------------------------------------------00
 //00---------------- EX Boolean - Javascripts ----------------------00
 //00----------------------------------------------------------------00
 //00000000000000000000000000000000000000000000000000000000000000000000
@@ -306,5 +70,141 @@
 // --------------- Ex Quanti minuti mancano alla fine dell'ora? ----------------
 // --------------- Inserisci due parole, qual'è la più lunga? ------------------
 // ------------------------ Ex Rock paper scissor ------------------------------
+
+
+/**********************************************************
+ ************       ARRAY EXERISE       *******************
+ *********************************************************/
+
+// var nomi = ["Luca", "Marco", "Paolo"];
+// for (var i = 0; i < nomi.length; i++){
+// console.log(nomi[i]);
+// }
+
+
+
+
+/* 1-     Chiedi all’utente la sua email controlla che sia
+nella lista di chi può accedere e stampa un messaggio appropriato */
+
+// let email = ['ciao@gmail.com', 'daje@gmail.com', 'pippo@gmail.com'];
+
+// function checkEmail() {
+
+//     let emailUser = prompt('Inserisci la tua email')
+//     let authUser = false;
+
+//     console.log(emailUser);
+//     console.log(email);
+//     console.log(authUser);
+    
+//     for (let a = 0; a < email.length; a++) {
+        
+//        if (email[a] ==  emailUser ) {
+
+//         authUser = true;
+//         }
+//     };
+
+//     if (authUser == true) {
+
+//         document.write('Accesso Consentito');
+
+//     } else {
+
+//         document.write('Non puoi Accedere, riprova')
+//     }
+
+//     return;
+  
+// };
+
+// checkEmail();
+
+/* 2- Il gioco dei dadi, chi fà di più vince */
+
+
+// let nUser1 = getRandom(1, 15);
+// let computer = getRandom(1, 15);
+// let numeri = [];
+
+// numeri.push(nUser1, computer)
+// console.log(numeri);
+
+// let a = numeri[0];
+// console.log(a);
+
+// let b = numeri[1];
+// console.log(b);
+
+
+
+// function getRandom(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min)) + min;
+// }
+
+
+// let giocata = () => {
+//     if (a < b){
+        
+//         document.write('Hai Perso')
+
+//     } else if (a > b) {
+        
+//         document.write('Hai vinto!')
+
+//     } else if (a == b) {
+//         document.write('pareggio')
+//     }
+// }
+
+// giocata();
+
+/* 3 - Chiedi all’utente il cognome, inseriscilo in un
+array con altri cognomi e stampa la lista
+ordinata alfabeticamente. Scrivi a che
+posizione della lista il nuovo utente si trova */
+
+
+// let surname = ['rossi', 'bianchi', 'mare'];
+
+// console.log(surname);
+
+// let surnameUser = prompt('Inserisci il tuo cognome!');
+
+// console.log(surname);
+
+
+// function insertSurname(surnameUser) {
+
+//     if (surnameUser != ('' || 'surname') ) {
+
+//         surname.push(surnameUser)
+
+//         orderSurname = surname.sort();
+
+//         console.log(surname);
+        
+//     } else {
+//         console.log('Il cognome non è valido');
+//     };
+
+//     return
+// }
+
+// function showSurname() {
+//     for (let a = 0; a < surname.length; a++) {
+
+//         b = '<li> '  + surname[a] + ' ' + surname.indexOf(surname[a]) + ' </li>';
+//         document.write(b);
+//      };
+
+//      return;
+// };
+
+// insertSurname(surnameUser);
+// showSurname();
 
 
